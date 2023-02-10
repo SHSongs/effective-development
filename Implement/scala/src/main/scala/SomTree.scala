a1aa92389cda858c15703031cecfaa7379b99f35
som <solver.som@gmail.com>
2023-02-10 22:32:26 +0900

    탐색

--- Implement/scala/src/main/scala/SomTree.scala
+++ Implement/scala/src/main/scala/SomTree.scala
@@ -1,17 +1,30 @@
 import scala.collection.mutable.ArrayBuffer
 
-case class Node[T](t: T, l: Option[Node[T]] = None, r: Option[Node[T]] = None)
-
+trait Node[T <: AnyVal]
+final case class IntNode(t: Int, l: Option[IntNode] = None, r: Option[IntNode] = None) extends Node[Int]
+//case object IntNode extends Node[Int]
 object SomTree {
-  def preorder[T](root: Node[T]): Seq[T] = {
-    val acc = ArrayBuffer[T]()
-    def run(node: Node[T]): Unit = {
-      print(node.t)
-      acc.append(node.t)
-      if (node.l.isDefined) run(node.l.get)
-      if (node.r.isDefined) run(node.r.get)
-    }
-    run(root)
-    acc.toSeq
+
+  def find(target: Int, root: IntNode): Option[IntNode] = {
+    def run(node: Option[IntNode]): Option[IntNode] =
+      node match
+        case Some(n) if n.t == target => node
+        case Some(n) if n.t > target => run(n.l)
+        case Some(n) if n.t < target => run(n.r)
+        case None => None
+
+    run(Some(root))
   }
+
+//  def preorder[T](root: Node[T]): Seq[T] = {
+//    val acc = ArrayBuffer[T]()
+//    def run(node: Node[T]): Unit = {
+//      print(node.t)
+//      acc.append(node.t)
+//      if (node.l.isDefined) run(node.l.get)
+//      if (node.r.isDefined) run(node.r.get)
+//    }
+//    run(root)
+//    acc.toSeq
+//  }
 }
--- Implement/scala/src/test/scala/TreeSpec.scala
+++ Implement/scala/src/test/scala/TreeSpec.scala
@@ -2,9 +2,13 @@ import org.scalatest.{Suite, Suites}
 import org.scalatest.funsuite.AnyFunSuite
 
 class TreeSpec extends AnyFunSuite:
-  val sampleTree = Node("A", Some(Node("B", Some(Node("D")), Some(Node("E")))), Some(Node("C")))
+//  val sampleTree = Node("A", Some(Node("B", Some(Node("D")), Some(Node("E")))), Some(Node("C")))
+  val sampleIntTree = IntNode(50, Some(IntNode(30, Some(IntNode(10)), Some(IntNode(40)))), Some(IntNode(60)))
 
-  test("preorderSpec") {
-    assert(Seq("A", "B", "D", "E", "C") === SomTree.preorder(sampleTree))
+  //  test("preorderSpec") {
+//    assert(Seq("A", "B", "D", "E", "C") === SomTree.preorder(sampleTree))
+//  }
+  test("find E") {
+    assert(SomTree.find(40, sampleIntTree) == Some(IntNode(40)))
   }
 
