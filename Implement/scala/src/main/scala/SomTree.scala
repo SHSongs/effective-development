bcd918eee10e0bbc9fcaaaf6ce7de54d1235ded7
som <solver.som@gmail.com>
2023-02-24 20:57:18 +0900

    step 1. string value index tree binary search

--- Implement/scala/src/main/scala/SomTree.scala
+++ Implement/scala/src/main/scala/SomTree.scala
@@ -1,9 +1,39 @@
 import scala.collection.mutable.ArrayBuffer
 
-trait Node[T <: AnyVal]
-final case class IntNode(t: Int, l: Option[IntNode] = None, r: Option[IntNode] = None) extends Node[Int]
+trait Node[T] extends Ordered[T]
+final case class IntNode(t: Int, l: Option[IntNode] = None, r: Option[IntNode] = None) extends Node[Int] {
+  override def compare(that: Int): Int = t.compare(that)
+}
+
+final case class IV[T <: String](i: Int, t:T)
+
+final case class IndexNode[T <: String](iv: IV[T], l: Option[IndexNode[T]] = None, r: Option[IndexNode[T]] = None) extends Node[T] {
+  override def compare(that: T): Int = iv.t.compareTo(that)
+}
 //case object IntNode extends Node[Int]
 object SomTree {
+
+  def findIndex[T <: String](target: Int, root: IndexNode[T]): Option[IndexNode[T]] = {
+    def run(node: Option[IndexNode[T]]): Option[IndexNode[T]] =
+      node match
+        case Some(n) if n.iv.i == target => node
+        case Some(n) if n.iv.i > target => run(n.l)
+        case Some(n) if n.iv.i < target => run(n.r)
+        case None => None
+
+    run(Some(root))
+  }
+
+  def find[T <: String](target: T, root: IndexNode[T]): Option[IndexNode[T]] = {
+    def run(node: Option[IndexNode[T]]): Option[IndexNode[T]] =
+      node match
+        case Some(n) if n.iv.t == target => node
+        case Some(n) if n.iv.t > target => run(n.l)
+        case Some(n) if n.iv.t < target => run(n.r)
+        case None => None
+
+    run(Some(root))
+  }
 
   def find(target: Int, root: IntNode): Option[IntNode] = {
     def run(node: Option[IntNode]): Option[IntNode] =
--- Implement/scala/src/test/scala/TreeSpec.scala
+++ Implement/scala/src/test/scala/TreeSpec.scala
@@ -5,6 +5,7 @@ class TreeSpec extends AnyFunSuite:
 //  val sampleTree = Node("A", Some(Node("B", Some(Node("D")), Some(Node("E")))), Some(Node("C")))
   val sampleIntTree = IntNode(50, Some(IntNode(30, Some(IntNode(10)), Some(IntNode(40)))), Some(IntNode(60)))
 
+  val sampleIndexTree = IndexNode[String](IV(50, "ㄹ"), Some(IndexNode(IV(30, "ㄴ"), Some(IndexNode(IV(10, "ㄱ"))), Some(IndexNode(IV(40, "ㄷ"))))), Some(IndexNode(IV(60, "ᆼ"))))
   //  test("preorderSpec") {
 //    assert(Seq("A", "B", "D", "E", "C") === SomTree.preorder(sampleTree))
 //  }
@@ -12,8 +13,19 @@ class TreeSpec extends AnyFunSuite:
     assert(SomTree.find(40, sampleIntTree) == Some(IntNode(40)))
   }
 
+  test("find index") {
+    val expect = Some(IndexNode(IV(40, "ㄷ")))
+    assert(SomTree.findIndex(40, sampleIndexTree) == expect)
+  }
+
+  test("find") {
+    val expect = Some(IndexNode(IV(40, "ㄷ")))
+    assert(SomTree.find("ㄷ", sampleIndexTree) == expect)
+  }
+
   test("count age > 25"){
     val res = SomList.filter(User.makeUsers, _.age > 25)
     assert(res.length == 2)
   }
+
 
